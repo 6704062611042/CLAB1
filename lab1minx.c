e2b496f7cb06ca6bba179774ea78872980fbc0da
6704062611042 <s6704062611042@email.kmutnb.ac.th>
2024-11-26 15:15:08 +0700

    commit 1 min max of 2 num

---
+++ lab1minx.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+	int var1 = 16;
+	int var2 = 8;
+	if (var1 > var2 ) {
+		printf("var1 is greater than var2");
+	} else {
+		printf("var2 is greater than var1");
+	}	
+	return 0;
+}
+
