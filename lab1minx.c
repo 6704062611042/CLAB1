ef42eb7d3a6060ab6a8c0e8c5f7bc0d06c928223
6704062611042 <s6704062611042@email.kmutnb.ac.th>
2024-11-26 15:23:21 +0700

    Commit 2
    เเก้จากข้อ 1 เป็นรับค่าจากคีย์บอร์ด

--- lab1minx.c
+++ lab1minx.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 int main() {
-	int var1 = 16;
-	int var2 = 8;
+	int var1 , var2;
+	scanf("%d" , &var1);
+	scanf("%d" , &var2);
 	if (var1 > var2 ) {
 		printf("var1 is greater than var2");
+		printf("\n%d", (var1-var2));
 	} else {
 		printf("var2 is greater than var1");
+		printf("\n%d", (var2-var1));
 	}	
 	return 0;
 }
-
