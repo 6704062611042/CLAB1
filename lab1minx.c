fd2541c8332986d8a73d6466d0859d7408cfd8ce
6704062611042 <s6704062611042@email.kmutnb.ac.th>
2024-12-03 13:19:15 +0700

    เพิ่มจากข้อ1 if if
    เปลี่ยนรูปเเบบเป็น if if

--- lab1minx.c
+++ lab1minx.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 int main() {
-	int var1;
-	int var2;
-	scanf("%d" , &var1);
-	scanf("%d" , &var2);
+	int var1 = 10;
+	int var2 = 16;
 	if (var1 > var2 ) {
 		printf("var1 is greater than var2");
-		printf("\n%d", (var1-var2));
-	} else {
+	} 
+	if (var2 > var1) {
 		printf("var2 is greater than var1");
-		printf("\n%d", (var2-var1));
 	}	
 	return 0;
 }
